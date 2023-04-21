f27b82159ef6e6df16dab5384a22f217b17e84be
Pollin <christopher.pollin@uni-graz.at>
2023-04-21 12:49:42 +0200

    update

--- WebDev/WebDev_0_JavaScript_Introduction/js_chatgpt/mood color picker bootstrap5 extended/app.js
+++ WebDev/WebDev_0_JavaScript_Introduction/js_chatgpt/mood color picker bootstrap5 extended/app.js
@@ -1,27 +1,52 @@
+let count = 0;
+
 function changeMoodColor() {
     const mood = document.getElementById('mood').value;
     let color;
     const hungry_color = '#C96115';
+    
 
     switch (mood) {
         case 'happy':
             color = '#FFD700';
+            count++;
             break;
         case 'sad':
             color = '#4169E1';
+            count++;
             break;
         case 'energetic':
             color = '#FF4500';
+            count++;
             break;
         case 'calm':
             color = '#228B22';
+            count++;
             break;
         case 'hungry':
 		    color = hungry_color;
+            count++;
 		    break;
         default:
             color = '#f0f0f0';
     }
 
+    if(mood == "happy" || mood == "calm")
+    {
+        displayInfo(mood);
+    }
+    
     document.body.style.backgroundColor = color;
+    console.log("Count ist " + count);
+    console.log("count: " + typeof count);
+    console.log("color: " + typeof color);
+    console.log("mood: " + typeof mood);
+    console.log("document.getElementById('mood'): " + typeof document.getElementById('mood'));
 }
+
+
+function displayInfo(mood) {
+    document.getElementById('info').innerText = mood;
+}
+
+
